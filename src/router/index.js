21c455219aeff422ffd620b2f4f87167c258d5e2
Vadim Demchenko <lionkenedi95@gmail.com>
2020-01-11 16:29:51 +0300

    Init project. Add main styles, routes and views

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,24 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
+import EnterView from "../views/Enter.vue";
+const LoginView = () =>
+  import(/* webpackChunkName: "loginview" */ "../views/Login.vue");
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    name: "enter",
+    component: EnterView
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/login",
+    name: "login",
+    component: LoginView
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
   base: process.env.BASE_URL,
   routes
 });
---
+++ src/views/Enter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="enterview">
+    <h1>Ready for a great User experience?</h1>
+    <p>Bring your media to the next level!</p>
+    <div>
+      <a :to="{ name: 'signupview' }" class="button">Sign Up</a>
+      <a :to="{ name: 'loginview' }" class="button button--dark">-></a>
+    </div>
+    <img src="../assets/woman.svg" alt="Woman image" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "enter"
+};
+</script>
+
+<style lang="scss" scoped>
+#enterview {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  img {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
+}
+</style>
